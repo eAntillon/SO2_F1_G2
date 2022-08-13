0e307c45b798904a7cbd3a12731b2a8cc0692b04
Edi Tomas <2370557320101@ingenieria.usac.edu.gt>
2022-08-13 06:56:18 -0600

    Test
    it added a  code fragment to test

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
+	//"strings"
 )
 
 func main() {
-	fmt.Println("MetaOS")
+	fmt.Println("")
+	fmt.Println("################### WELCOME TO SYSTEM METAOS #########")
+	fmt.Println("|----------------------------------------------------|")
+	fmt.Println("|    Enter command or exit to finish the execution:  |")
+	fmt.Println("|----------------------------------------------------|")
+	fmt.Println("")
+	for {
+		fmt.Print(">> ")
+		com := bufio.NewScanner(os.Stdin)
+		if com.Scan() {
+			if com.Text() == "exit" {
+				break
+			} else {
+				//strace(strings.Fields(com.Text()))
+			}
+		}
+	}
 }
