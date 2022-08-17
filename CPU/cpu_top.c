073433fc05314e2221e8e4073be08d8b01d63263
201503702 <201503702@ingenieria.usac.edu.gt>
2022-08-16 20:10:11 -0600

    Update cpu_top
    Se cambia el contenido del archivo para colocar un JSON en carpeta proc, un archivo llamado proc.json

--- CPU/cpu_top.c
+++ CPU/cpu_top.c
@@ -1,201 +1,115 @@
-    #include <linux/fs.h>
-    #include <linux/init.h>
-    #include <linux/kernel.h>
-    #include <linux/sched.h>
-    #include <linux/sched/signal.h>
-    #include <linux/module.h>
-    #include <linux/seq_file.h>
-    #include <linux/proc_fs.h>
-    
-    int procesoEjecucion = 0;
-    int procesoSuspendidos = 0;
-    int procesoDetenido = 0;
-    int procesoZombie = 0;
-    int procesosTotales = 0;
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/sched/signal.h>
 
-	
-    char estados(volatile long estado )
-    {   
-        procesosTotales = procesosTotales + 1;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Edgar Borrayo - 201503702");
+MODULE_DESCRIPTION("Basic process information Linux module.");
+MODULE_VERSION("0.1");
+
+
+int proc_count(void)
+{
+  int i=0;
+  struct task_struct *thechild;
+  for_each_process(thechild){
+    i++;
+
+  }
+  return i;
+}
+
+char buffer[256];
+char * get_task_state(long state)
+{
+    switch (state) {
+        case TASK_RUNNING:
+            return "TASK_RUNNING";
+        case TASK_INTERRUPTIBLE:
+            return "TASK_INTERRUPTIBLE";
+        case TASK_UNINTERRUPTIBLE:
+            return "TASK_UNINTERRUPTIBLE";
+        case TASK_REPORT_IDLE:
+            return "TASK_REPORT_IDLE";
+        case TASK_IDLE:
+            return "TASK_IDLE";
+        case __TASK_STOPPED:
+            return "__TASK_STOPPED";
+        case __TASK_TRACED:
+            return "__TASK_TRACED";
+        default:
+        {
+            sprintf(buffer, "Unknown Type:%ld\n", state);
+            return buffer;
+        }
+    }
+}
+
+static int writeFile(struct seq_file* archivo, void *v){
+    int i = 0;
+    bool comma = false;
+    struct task_struct *thechild;
+
+    seq_printf(archivo, "[\n");
+    for_each_process(thechild){
         
-        if(estado == 0)
-        {
-            procesoEjecucion = procesoEjecucion + 1;
-            return 'R';
-        }
-
-        if(estado== 1)
-        {
-            procesoSuspendidos = procesoSuspendidos + 1;
-            return 'S';
-        }
-        
-        if(estado== 2)
-        {
-            procesoSuspendidos = procesoSuspendidos + 1;
-            return 'D';
-        }
-
-        if(estado== 4)
-        {
-            procesoZombie = procesoZombie + 1;
-            return 'Z';
-        }
-
-        if(estado== 8)
-        {
-            procesoDetenido = procesoDetenido + 1;
-            return 'T';
-        }
-
-        if(estado == 32)
-        {            
-            return 'X';
-        }
-
-        procesoEjecucion = procesoEjecucion + 1;
-        return 'E';
+        if (comma) {
+            seq_printf(
+                archivo, 
+                ",\n\t{\"UID\": %d, \"PID\": %d, \"Padre\": %d, \"Process\": \"%s\", \"State\": \"%s\"}", 
+                thechild->real_cred->uid.val, 
+                thechild->pid, 
+                thechild->real_parent->pid, 
+                thechild->comm, 
+                get_task_state(thechild->__state)
+            );
+        }else{
+            seq_printf(
+                archivo, 
+                "\t{\"UID\": %d, \"PID\": %d, \"Padre\": %d, \"Process\": \"%s\", \"State\": \"%s\"}", 
+                thechild->real_cred->uid.val, 
+                thechild->pid, 
+                thechild->real_parent->pid, 
+                thechild->comm, 
+                get_task_state(thechild->__state)
+            );  
+            comma = true;  
+        }    
+        i++;
     }
+    seq_printf(archivo, "\n]\n");
+    pr_info("Number of processes: %u\n", i);
 
-    static int meminfo_proc_show(struct seq_file *m, void *v){
-		int contador = 0;
-		long memoria = 0;
-		long cpu = 0;
-        int contadorHijos = 0; //0: no coloque tag, 1: ya coloque tag
+    return 0;
+}
 
-		struct task_struct *tarea;          //El task struct es para los procesos, deberia haber algo similar para memoria y cpu
-        struct task_struct *task_child;     /*    Structure needed to iterate through task children    */
-        struct list_head *list;             /*    Structure needed to iterate through the list in each task->children struct    */
+static int atOpen(struct inode* inode, struct file* file){
+    return single_open(file, writeFile, NULL);
+}
 
-        seq_printf(m, "<table id=\"table\" class=\"jsTT\">");
-        seq_printf(m, "<tr><th>PID</th><th>Nombre</th><th>Estado</th><th>Memoria</th><th>ACCION</th></tr>");
-        
-        for_each_process(tarea) {
-			memoria=0;
-			cpu=0;
-			cpu = tarea->utime + tarea->stime;
+static struct proc_ops ops={
+    .proc_open = atOpen,
+    .proc_release = single_release,
+    .proc_read = seq_read,
+    .proc_lseek = seq_lseek
+};
 
-             if(tarea->mm){ 
-                 memoria = tarea->mm->total_vm * 100000 / 2048;
-                // memoria = (tarea->mm->mmap->vm_end - tarea->mm->mmap->vm_start );
-             }
-		  
-			/*if(tarea->mm){
-				memoria = tarea->mm->task_size;
-			}      */     
+static int load_module(void) {
+    printk(KERN_INFO "El grupo 2 ha montado el monitor de cpu\n");
+    printk(KERN_INFO "Total running processes: %d .\n", proc_count());
+    proc_create("proc.json", 0, NULL, &ops);
+    return 0;
+}
 
-            seq_printf(m, "<tr data-tt-id=\"%d\" data-tt-level=\"0\"> <td style=\"white-space: nowrap;\">   %d  </td>",
-                tarea->pid,
-                tarea->pid
-            );
+static void unload_module(void) {
 
-            seq_printf(m, "<td>%s</td>",
-                tarea->comm
-            );
+    printk(KERN_INFO "El grupo 2 ha removido el monitor de cpu.”\n");
+    remove_proc_entry("proc.json", NULL);
+}
 
-            seq_printf(m, "<td>%c</td>",
-                estados(tarea->__state)
-            );
-
-            seq_printf(m, "<td>%ld</td>",
-                memoria
-            ); 
-
-            seq_printf(m, "<td> <button type=\"button\" onclick=\"terminarProceso(%d)\">Kill</button></td>",
-            tarea->pid                
-            );          
-
-            seq_printf(m, "</tr>");
-
-        
-            list_for_each(list, &tarea->children){
-            
-            if(contadorHijos == 0){
-                    contadorHijos = contadorHijos + 1; 
-                }else{
-                    contadorHijos = contadorHijos + 1;
-                }
-            
-            task_child = list_entry( list, struct task_struct, sibling );
-            
-            seq_printf(m, "<tr data-tt-id=\"NA\" data-tt-parent-id=\"%d\"data-tt-level=\"1\"> <td style=\"white-space: nowrap;\"><span style=\"padding-left:0px;\"></span><span style=\"padding-left:0px;\"></span>   %d  </td>",
-                tarea->pid,
-                task_child->pid
-            );
-
-            seq_printf(m, "<td>%s</td>",
-                task_child->comm
-            );
-
-            seq_printf(m, "<td>%c</td>",
-                estados(task_child->__state)
-            );
-
-            seq_printf(m, "<td>%ld</td>",
-                memoria
-            ); 
-
-            seq_printf(m, "<td></td>"                
-            );          
-
-            seq_printf(m, "</tr>");
-
-         
-            }            
-                if(contadorHijos > 0){
-                    contadorHijos = 0;
-                } 
-			contador = contador + 1;
-        }
-            
-        seq_printf(m, "</table>");    
-        seq_printf(m, "<input id=\"procesoEjecucion\" name=\"procesoEjecucion\" type=\"hidden\" value=\"%d\">",
-        procesoEjecucion
-        );
-        seq_printf(m, "<input id=\"procesoSuspendidos\" name=\"procesoSuspendidos\" type=\"hidden\" value=\"%d\">",
-        procesoSuspendidos
-        );
-        seq_printf(m, "<input id=\"procesoDetenido\" name=\"procesoDetenido\" type=\"hidden\" value=\"%d\">",
-        procesoDetenido
-        );
-        seq_printf(m, "<input id=\"procesoZombie\" name=\"procesoZombie\" type=\"hidden\" value=\"%d\">",
-        procesoZombie
-        );
-        seq_printf(m, "<input id=\"procesosTotales\" name=\"procesosTotales\" type=\"hidden\" value=\"%d\">",
-        procesosTotales
-        );
-        return 0;
-    }
-       
-    static int meminfo_proc_open(struct inode *inode, struct file *file)
-    {
-        return single_open(file, meminfo_proc_show, NULL);
-    }
-
-    static const struct proc_ops meminfo_proc_ops = {
-        .proc_open       = meminfo_proc_open,
-        .proc_read       = seq_read,
-        .proc_lseek     = seq_lseek,
-        .proc_release    = single_release,
-    };
-
-    
-    static int __init inicio_mod(void)
-    {
-		//Este metodo escribe el archivo en la carpeta PROC
-        printk(KERN_INFO "Nombre: Practica1 G2 \n");
-        proc_create("cpu_top", 0, NULL, &meminfo_proc_ops);
-        return 0;
-    }
-
-	static void __exit final_mod(void)
-    {   
-		//Este metodo sale del modulo
-        printk(KERN_INFO "Descargando: Diciembre2020 \n");
-        remove_proc_entry("cpu_top",NULL);
-    }
-
-    module_init(inicio_mod);
-    module_exit(final_mod);
-
-	MODULE_LICENSE("GPL");
+module_init(load_module);
+module_exit(unload_module);
